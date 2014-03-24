67f7aa13cb54f93a88baa5300a550b386ccc84b3
Will Anderson <will@itsananderson.com>
2014-03-23 19:34:29 -0700

    Switch to Express from custom static server

--- bin/server.js
+++ bin/server.js
@@ -7,8 +7,9 @@ var program = require('commander');
 program
     .version('1.0.1')
     .option('-p, --port [port]', 'Server Port (Defaults to 8000)')
+    .option('-r, --root [root]', 'Server Root (Defaults to ./)')
     .parse(process.argv);
 
 process.title = 'web-server';
 
-require('../lib/web-server')(program.port || 8000);
+require('../lib/web-server')(program.port || 8000, program.root || './');
