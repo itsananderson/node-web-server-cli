7b4d653ce67f0a1c7df2ba96af22b5ac68a8a133
Will Anderson <will@itsananderson.com>
2014-05-07 17:11:17 -0700

    Add default index file handler

--- bin/server.js
+++ bin/server.js
@@ -5,11 +5,12 @@
 var program = require('commander');
 
 program
-    .version('1.0.1')
+    .version('0.2.0')
     .option('-p, --port [port]', 'Server Port (Defaults to 8000)')
     .option('-r, --root [root]', 'Server Root (Defaults to ./)')
+    .option('-i, --index [index]', 'Index file (Defaults to index.html)')
     .parse(process.argv);
 
 process.title = 'web-server';
 
-require('../lib/web-server')(program.port || 8000, program.root || './');
+require('../lib/web-server')(program.port || 8000, program.root || './', program.index || 'index.html');
