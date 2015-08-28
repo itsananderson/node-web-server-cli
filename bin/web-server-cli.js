059748f74684c9730f1ccd3f89a192c2da1e49f0
Will Anderson <will@itsananderson.com>
2015-08-27 17:15:24 -0700

    Allow index to be disabled, or to be multiple files

--- bin/web-server-cli.js
+++ bin/web-server-cli.js
@@ -7,12 +7,20 @@ var program = require('commander'),
 
 program
     .version(pkg.version)
-    .option('-p, --port [port]', 'Server Port (Defaults to 8000)')
-    .option('-r, --root [root]', 'Server Root (Defaults to current directory)')
-    .option('-i, --index [index]', 'Index file (Defaults to index.html)')
-    .option('-h, --hidden', 'Show hidden files')
+    .option('-p, --port [port]', 'Server Port (Defaults to 8000)', 8000)
+    .option('-r, --root [root]', 'Server Root (Defaults to current directory)', './')
+    .option('-i, --index <index>', 'Index file (Defaults to index.html)', 'index.html')
+    .option('-h, --hidden', 'Show hidden files', false)
+    .option('-I, --disable-index', "Disable index files", false)
     .parse(process.argv);
 
 process.title = 'web-server';
 
-require('../lib/web-server')(program.port || process.env.PORT || 8000, program.root || './', program.index || 'index.html', program.hidden);
+var index;
+if (program.disableIndex) {
+    index = [];
+} else {
+    index = program.index.split(",");
+}
+
+require('../lib/web-server')(program.port, program.root, index, program.hidden);
